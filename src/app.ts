2acb5da46ae3a58b1dd23355b7870daee142b443
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2026-09-24 09:57:43 -0300

    Adding notFound middleware

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@
 // Contains express related logic
 
 import express from 'express';
+import { errorHandler } from './middlewares/errorHandler';
+import { notFound } from './middlewares/notFound';
 
 export function createApp(){
   const app = express();
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(errorHandler);
+  app.use(notFound);
 };
